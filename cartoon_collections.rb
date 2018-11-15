c8d73211c048a4bc3df72b8c011a54e699aa053d
Ethan Rosenberg <ethan@ethanrosenberg.com>
2018-11-15 21:44:19 +0000

    automatically backed up by learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -1,5 +1,7 @@
-def roll_call_dwarves# code an argument here
-  # Your code here
+def roll_call_dwarves(dwarves)
+  dwarves.each_with_index { |dwarve, index|
+    puts dwarve + " " + index
+  }
 end
 
 def summon_captain_planet# code an argument here
